c7fb485a26588993c754c722a3e97ad630b0176b
michaellones <michaellones@localhost>
2022-09-22 15:10:48 +0100

    Initial commit

---
+++ src/DegreeAdvice.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Software Development 1, Coursework 1.
+ * 
+ * Make sure you read the specification document on
+ * Canvas in full before starting this assessment.
+ * 
+ */
+public class DegreeAdvice {
+
+	public static void main(String[] args) {
+		
+		// add your code here!
+
+	}
+
+}
